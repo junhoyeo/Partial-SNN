45739db52a2a51555f904030a23df994e1758909
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-26 19:13:56 +0900

    refactor: rename DEFAULT to HIDDEN

--- network.py
+++ network.py
@@ -3,8 +3,8 @@ from pprint import pprint
 
 model = [
   [Node(node_types.INPUT)] * 2,
-  [Node(node_types.DEFAULT)] * 3,
-  [Node(node_types.DEFAULT)] * 2,
+  [Node(node_types.HIDDEN)] * 3,
+  [Node(node_types.HIDDEN)] * 2,
   [Node(node_types.OUTPUT)] * 1,
 ]
 
--- schemas/node_types.py
+++ schemas/node_types.py
@@ -1,10 +1,10 @@
 class NodeTypes:
   def __init__(self):
     self.INPUT = 0
-    self.DEFAULT = 1
+    self.HIDDEN = 1
     self.OUTPUT = 2
 
   def get_repr(self, type_id: int):
-    return ['INPUT', 'DEFAULT', 'OUTPUT'][type_id]
+    return ['INPUT', 'HIDDEN', 'OUTPUT'][type_id]
 
 node_types = NodeTypes()
